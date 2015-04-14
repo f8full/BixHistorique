adc64f7b21143d8e5aaaca7cf0988327ba2c6c16
F8Full <f8full@gmail.com>
2015-04-14 14:44:06 -0400

    GENESIS TROUBLE (bis repetita)
    I somehow missed that file, so here goes the real deployed version
    
    ==REPEATED PREVIOUS COMMIT MESSAGE FOLLOWS==
    Genesis my ass. I couldn't setup CRON jobs the way I originally intended so I came up with a more flexible approach.
    
    ** this is the version that will be deployed on all instances with specific time setup**
    
    Giving you :
    
    Parsing status control
    
    The whole parsing process can now  be activated by making calls to specific Cron URLs (requires administrator privileges)
    As follow
    http://localshost:8080/cron/activateparsingcronjob?activate=true
    http://localshost:8080/cron/activateparsingcronjob?activate=false
    
    By default, parsing_active is FALSE, meaning parsing job will be called but do nothing.
    It is so because the cron.xml entries for the activation job are commented. To setup autocron in a DEPLOYED environment (no support from GAE for cron in local environment, just call the URLs yourself, or have your own cron doing it)
    
    You can check the parsing_active current status via a request on the API (getstats, (and future isparsingactive)).
    
    I added an entity to the datastore to track the status and log some data about the parsing process. It's all available through the API endpoint method getstats
    
    Future refactoring : clean the mess I made between DataStats and ParsingStatus. I also could optimise the read cost of getstats by not having to pass queries to grab times of oldest/newest records. I leave it as is right now given time constraints and the fact getstats is not expected to be called that often.

---
+++ backend/src/main/java/com/F8Full/bixhistorique/backend/datamodel/dataStats.java
@@ -0,0 +1,31 @@
+package com.F8Full.bixhistorique.backend.datamodel;
+
+import java.util.Date;
+
+/**
+ * Created by F8Full on 2015-04-13.
+ * Class data for stats. TODO: Refactor with ParsingStatus to improve getstats reads cost (no datastore query)
+ */
+public class DataStats {
+
+
+    public String availability_refresh_rate = "5min";
+    public String stationproperties_refresh_rate = "24h 03:00 America/Montreal";
+    public Date oldest_availability_date;
+    public Date stats_generated_on_datetime = new Date();
+    public long nb_total_entities;
+    public long nb_network_entities;
+    public long nb_stationproperties_entities;
+
+    public int last_complete_availability_nb_stations;
+    public Date last_complete_availability_date;
+
+    public Date last_partial_availability_date;
+    public int last_partial_availability_nb_stations;
+
+    public boolean parsing_active;
+    public long nb_complete_availability_parse;
+    public long nb_partial_availability_parse;
+    public long total_nb_availability_parse;
+    public long nb_days_of_data;
+}
